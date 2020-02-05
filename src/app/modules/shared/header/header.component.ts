76a276e18143caae47b958dc3da3d95a3a340b94
innovatorsnest <innovatorsnest@gmail.com>
2020-02-06 00:27:23 +0530

    [FIX] added the navigation

--- src/app/modules/shared/header/header.component.ts
+++ src/app/modules/shared/header/header.component.ts
@@ -7,9 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+
+  itemSelected;
+
+  pages =  [
+    {name: 'Home', link: 'home', active: false},
+    {name: 'About Us', link: 'about', active: false},
+    {name: 'Our Services', link: 'services', active: false},
+    {name: 'Features', link: 'features',active: false},
+    {name: 'Testimonals', link: 'testimonals', active: false},
+    {name: 'Contact', link: 'contact'},
+  ];
   constructor() { }
 
   ngOnInit() {
+  }
+
+  itemClicked(item) {
+    console.log('item', item);
+    this.itemSelected = {...item};
+    console.log('item selected', this.itemSelected);
   }
 
 }
