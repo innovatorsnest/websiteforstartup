d5a436c16a7804a97b8b2d313d81074c3cd16e33
innovatorsnest <innovatorsnest@gmail.com>
2020-02-06 16:35:03 +0530

    [FIX] added the files for deploy

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
+//Install express server
 const express = require('express');
-const app = express();
 const path = require('path');
 
+const app = express();
 
-console.log(path.join(__dirname, 'dist','websiteforstartup','index.html'));
-
-app.use(express.static(__dirname + '/dist'));
-
-app.listen(process.env.PORT || 8080);
+// Serve only the static files form the angularapp directory
+app.use(express.static(__dirname + '/websiteforstartup'));
 
 app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname, 'dist','websiteforstartup','index.html'));
-})
 
-console.log('working express');
+res.sendFile(path.join(__dirname+'/websiteforstartup/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
