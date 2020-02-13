b597762a5eb674031855cc6b28206d6ba027fbc4
innovatorsnest <innovatorsnest@gmail.com>
2020-02-13 16:43:23 +0530

    [FIX] added the necessary information for the website completion

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -14,24 +14,27 @@ export class HomeComponent implements OnInit {
     {
       image: '../../../assets/images/sldier-images/slider_01.jpg',
       title: 'Progress your e-trade Business with us !',
-      subtitle: 'Grow your business to a new level with our complete E-commerce Solutions.'
+      subtitle: 'Grow your business to a new level with our complete E-commerce Solutions.',
+      button: 'Get In touch',
     },
     {
       image: '../../../assets/images/sldier-images/slider_02.jpg',
       title: 'we guarantee',
-      subtitle: 'Your business growth'
+      subtitle: 'Your business growth',
+      button: 'Contact Us'
     },
     {
       image: '../../../assets/images/sldier-images/slider_03.jpg',
       title: 'Connect with us',
-      subtitle: 'get your free consultation report'
+      subtitle: 'get your free consultation report',
+      button: 'Get Started Now'
     }
   ];
   showIndicator = true;
 
   constructor(carauselConfig: CarouselConfig) {
 
-    carauselConfig.interval = 1500;
+    carauselConfig.interval = 2000;
     carauselConfig.noPause = true;
     carauselConfig.showIndicators = true;
   }
